378c1da71f7d15812ed05c8260afe57da4792964
Kushagra Seth <kseth919@gmail.com>
2019-11-24 23:17:53 +0530

    Updates

---
+++ precache-manifest.06d1e9966df3d8b8c243f545185dfab5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f478d12f54192f909843618a9630fac",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/RoboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "465fdd6b40e5a3571c73",
+    "url": "/RoboFriends/static/css/main.cbfa73f3.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/RoboFriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "465fdd6b40e5a3571c73",
+    "url": "/RoboFriends/static/js/main.b3541c73.chunk.js"
+  },
+  {
+    "revision": "6239f40d0ef81a2fd1e0",
+    "url": "/RoboFriends/static/js/runtime-main.6d5bc549.js"
+  },
+  {
+    "revision": "ee444c3cc1cd133934ccc272261e3824",
+    "url": "/RoboFriends/static/media/CFAnarchy-Regular.ee444c3c.woff"
+  }
+]);
